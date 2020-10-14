550bd3f65813545abc0e8069248f2494d44132c9
IamRandom-Nameless <101219946@students.swinburne.edu.my>
2020-10-14 20:28:46 +0800

    update program by including obstacles and snake length

--- SnakeGame/SnakeGame/Program.cs
+++ SnakeGame/SnakeGame/Program.cs
@@ -28,117 +28,173 @@ namespace SnakeGame
 		}
 
 	}
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // start game
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
 
-            // display this char on the console during the game
-            char ch = '*';
-            bool gameLive = true;
-            ConsoleKeyInfo consoleKey; // holds whatever key is pressed
+	class Obstacle
+	{
+		public int ObstacleX;
+		public int ObstacleY;
+		public Obstacle(int obX, int obY)
+		{
+			ObstacleX = obX;
+			ObstacleY = obY;
+		}
 
-            // location info & display
-            int x = 0, y = 2; // y is 2 to allow the top row for directions & space
-            int dx = 1, dy = 0;
-            int consoleWidthLimit = 79;
-            int consoleHeightLimit = 24;
+		public int obX
+		{
+			get { return ObstacleX; }
+			set { ObstacleX = value; }
 
-            // clear to color
-            Console.BackgroundColor = ConsoleColor.DarkGray;
-            Console.Clear();
+		}
 
-            // delay to slow down the character movement so you can see it
-            int delayInMillisecs = 50;
+		public int obY
+		{
+			get { return ObstacleY; }
+			set { ObstacleY = value; }
 
-            // whether to keep trails
-            bool trail = false;
+		}
+
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// start game
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadKey();
+
+			// display this char on the console during the game
+			string character = "***";
+			var ch = character.ToCharArray();
+			bool gameLive = true;
+			ConsoleKeyInfo consoleKey; // holds whatever key is pressed
+
+			// location info & display
+			int x = 0, y = 2; // y is 2 to allow the top row for directions & space
+			int dx = 1, dy = 0;
+			int consoleWidthLimit = 79;
+			int consoleHeightLimit = 24;
+
+			// clear to color
+			Console.BackgroundColor = ConsoleColor.DarkGray;
+			Console.Clear();
+
+			// delay to slow down the character movement so you can see it
+			int delayInMillisecs = 50;
+
+			// whether to keep trails
+			bool trail = false;
+
 
 			//Food spawn
 			Random rand = new Random();
 			Food posi;
-			posi = new Food(rand.Next(0, consoleWidthLimit),
-								rand.Next(6, consoleHeightLimit));
+
+			posi = new Food(rand.Next(0, Console.WindowWidth),
+								rand.Next(0, Console.WindowHeight));
 			Console.SetCursorPosition(posi.foodX, posi.foodY);
 			Console.ForegroundColor = ConsoleColor.Cyan;
-			Console.Write("@");
+			Console.Write("+");
 
-            do // until escape
-            {
-                // print directions at top, then restore position
-                // save then restore current color
-                ConsoleColor cc = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.SetCursorPosition(0, 0);
-                Console.WriteLine("Arrows move up/down/right/left. Press 'esc' quit.");
-                Console.SetCursorPosition(x, y);
-                Console.ForegroundColor = cc;
+			// Obstacles spawn
 
-                // see if a key has been pressed
-                if (Console.KeyAvailable)
-                {
-                    // get key and use it to set options
-                    consoleKey = Console.ReadKey(true);
-                    switch (consoleKey.Key)
-                    {
-                      
-                        case ConsoleKey.UpArrow: //UP
-                            dx = 0;
-                            dy = -1;
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            break;
-                        case ConsoleKey.DownArrow: // DOWN
-                            dx = 0;
-                            dy = 1;
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            break;
-                        case ConsoleKey.LeftArrow: //LEFT
-                            dx = -1;
-                            dy = 0;
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            break;
-                        case ConsoleKey.RightArrow: //RIGHT
-                            dx = 1;
-                            dy = 0;
-                            Console.ForegroundColor = ConsoleColor.Black;
-                            break;
-                        case ConsoleKey.Escape: //END
-                            gameLive = false;
-                            break;
-                    }
-                }
+			Obstacle obst1 = new Obstacle(rand.Next(0, Console.WindowWidth), rand.Next(0, Console.WindowHeight));
+			Console.SetCursorPosition(obst1.ObstacleX, obst1.ObstacleY);
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+			Console.Write("||");
 
-                // find the current position in the console grid & erase the character there if don't want to see the trail
-                Console.SetCursorPosition(x, y);
-                if (trail == false)
-                    Console.Write(' ');
+			Obstacle obst2 = new Obstacle(rand.Next(0, Console.WindowWidth), rand.Next(0, Console.WindowHeight));
+			Console.SetCursorPosition(obst2.ObstacleX, obst2.ObstacleY);
+			Console.ForegroundColor = ConsoleColor.DarkGreen;
+			Console.Write("||");
 
-                // calculate the new position
-                // note x set to 0 because we use the whole width, but y set to 1 because we use top row for instructions
-                x += dx;
-                if (x > consoleWidthLimit)
-                    x = 0;
-                if (x < 0)
-                    x = consoleWidthLimit;
 
-                y += dy;
-                if (y > consoleHeightLimit)
-                    y = 2; // 2 due to top spaces used for directions
-                if (y < 2)
-                    y = consoleHeightLimit;
+			Obstacle obst3 = new Obstacle(rand.Next(0, Console.WindowWidth), rand.Next(0, Console.WindowHeight));
+			Console.SetCursorPosition(obst3.ObstacleX, obst3.ObstacleY);
+			Console.ForegroundColor = ConsoleColor.DarkBlue;
+			Console.Write("||");
 
-                // write the character in the new position
-                Console.SetCursorPosition(x, y);
-                Console.Write(ch);
 
-                // pause to allow eyeballs to keep up
-                System.Threading.Thread.Sleep(delayInMillisecs);
+			do // until escape
+			{
+				// print directions at top, then restore position
+				// save then restore current color
+				ConsoleColor cc = Console.ForegroundColor;
+				Console.ForegroundColor = ConsoleColor.Black;
+				Console.SetCursorPosition(0, 0);
+				Console.WriteLine("Arrows move up/down/right/left. Press 'esc' quit.");
+				Console.SetCursorPosition(x, y);
+				Console.ForegroundColor = cc;
 
-            } while (gameLive);
-        }
-    }
-    
+				// see if a key has been pressed
+				if (Console.KeyAvailable)
+				{
+					// get key and use it to set options
+					consoleKey = Console.ReadKey(true);
+					switch (consoleKey.Key)
+					{
+
+						case ConsoleKey.UpArrow: //UP
+							dx = 0;
+							dy = -1;
+							Console.ForegroundColor = ConsoleColor.Red;
+							break;
+						case ConsoleKey.DownArrow: // DOWN
+							dx = 0;
+							dy = 1;
+							Console.ForegroundColor = ConsoleColor.Cyan;
+							break;
+						case ConsoleKey.LeftArrow: //LEFT
+							dx = -1;
+							dy = 0;
+							Console.ForegroundColor = ConsoleColor.Green;
+							break;
+						case ConsoleKey.RightArrow: //RIGHT
+							dx = 1;
+							dy = 0;
+							Console.ForegroundColor = ConsoleColor.Black;
+							break;
+						case ConsoleKey.Escape: //END
+							gameLive = false;
+							break;
+					}
+
+				}
+
+
+
+				// find the current position in the console grid & erase the character there if don't want to see the trail
+				Console.SetCursorPosition(x, y);
+				if (trail == false)
+					Console.Write(' ');
+				Console.Write(' ');
+				Console.Write(' ');
+
+				// calculate the new position
+				// note x set to 0 because we use the whole width, but y set to 1 because we use top row for instructions
+				x += dx;
+				if (x > consoleWidthLimit)
+					x = 0;
+				if (x < 0)
+					x = consoleWidthLimit;
+
+				y += dy;
+				if (y > consoleHeightLimit)
+					y = 2; // 2 due to top spaces used for directions
+				if (y < 2)
+					y = consoleHeightLimit;
+
+				// write the character in the new position
+				Console.SetCursorPosition(x, y);
+				Console.Write(ch);
+
+
+
+				// pause to allow eyeballs to keep up
+				System.Threading.Thread.Sleep(delayInMillisecs);
+
+			} while (gameLive);
+		}
+	}
+
 }
